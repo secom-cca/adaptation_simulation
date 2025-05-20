317a4ed801bb7825d00c6734f675855af90f467a
OjiTa2000 <ojima-takuto572@g.ecc.u-tokyo.ac.jp>
2025-05-20 12:16:09 +0900

    change layout

--- results/js/result_script.js
+++ results/js/result_script.js
@@ -3,7 +3,7 @@ var yourname = "あなたの名前"
 var scenarioname = ["RCP0.00"];
 var policyname = ["施策名"]
 var eventname = ["イベント名"]
-var scorename = ["洪水被害", "農業生産", "都市利便性", "生態系", "住民負担"]
+var scorename = ["洪水被害", "農業生産", "住民負担", "生態系", "都市利便性"]
 var score2050 = [10,20,30,40,50];
 var score2075 = [90,80,70,60,50];
 var score2100 = [75,37,73,15,95];
@@ -45,26 +45,26 @@ document.getElementById("scorename2100_3").innerText=scorename[3];
 document.getElementById("scorename2100_4").innerText=scorename[4];
 
 
-document.getElementById("res2050_0").innerText=score2050[0];
-document.getElementById("res2050_1").innerText=score2050[1];
-document.getElementById("res2050_2").innerText=score2050[2];
-document.getElementById("res2050_3").innerText=score2050[3];
-document.getElementById("res2050_4").innerText=score2050[4];
-document.getElementById("res2050_sum").innerText=sum2050;
+document.getElementById("res2050_0").innerText=score2050[0].toFixed(2);
+document.getElementById("res2050_1").innerText=score2050[1].toFixed(2);
+document.getElementById("res2050_2").innerText=score2050[2].toFixed(2);
+document.getElementById("res2050_3").innerText=score2050[3].toFixed(2);
+document.getElementById("res2050_4").innerText=score2050[4].toFixed(2);
+document.getElementById("res2050_sum").innerText=sum2050.toFixed(2);
 
-document.getElementById("res2075_0").innerText=score2075[0];
-document.getElementById("res2075_1").innerText=score2075[1];
-document.getElementById("res2075_2").innerText=score2075[2];
-document.getElementById("res2075_3").innerText=score2075[3];
-document.getElementById("res2075_4").innerText=score2075[4];
-document.getElementById("res2075_sum").innerText=sum2075;
+document.getElementById("res2075_0").innerText=score2075[0].toFixed(2);
+document.getElementById("res2075_1").innerText=score2075[1].toFixed(2);
+document.getElementById("res2075_2").innerText=score2075[2].toFixed(2);
+document.getElementById("res2075_3").innerText=score2075[3].toFixed(2);
+document.getElementById("res2075_4").innerText=score2075[4].toFixed(2);
+document.getElementById("res2075_sum").innerText=sum2075.toFixed(2);
 
-document.getElementById("res2100_0").innerText=score2100[0];
-document.getElementById("res2100_1").innerText=score2100[1];
-document.getElementById("res2100_2").innerText=score2100[2];
-document.getElementById("res2100_3").innerText=score2100[3];
-document.getElementById("res2100_4").innerText=score2100[4];
-document.getElementById("res2100_sum").innerText=sum2100;
+document.getElementById("res2100_0").innerText=score2100[0].toFixed(2);
+document.getElementById("res2100_1").innerText=score2100[1].toFixed(2);
+document.getElementById("res2100_2").innerText=score2100[2].toFixed(2);
+document.getElementById("res2100_3").innerText=score2100[3].toFixed(2);
+document.getElementById("res2100_4").innerText=score2100[4].toFixed(2);
+document.getElementById("res2100_sum").innerText=sum2100.toFixed(2);
 
 
 
@@ -79,7 +79,7 @@ var Chart2050 = new Chart(ctx1, {
     //データの設定
     data: {
         //データ項目のラベル
-        labels: ["洪水被害", "農業生産", "都市利便性", "環境多様性", "住民負担"],
+        labels: scorename,
         //データセット
         datasets: [{
             label: "2050年", 
@@ -141,7 +141,7 @@ var Chart2075 = new Chart(ctx2, {
     //データの設定
     data: {
         //データ項目のラベル
-        labels: ["洪水被害", "農業生産", "都市利便性", "環境多様性", "住民負担"],
+        labels: scorename,
         //データセット
         datasets: [{
             label: "2075年", 
@@ -204,7 +204,7 @@ var Chart2100 = new Chart(ctx3, {
     //データの設定
     data: {
         //データ項目のラベル
-        labels: ["洪水被害", "農業生産", "都市利便性", "環境多様性", "住民負担"],
+        labels: scorename,
         //データセット
         datasets: [{
             label: "2100年", 
